0eef3a47f278ba012224e184dda3fd7e017498bd
Silas Souza <silasdacosta@gmail.com>
2021-11-13 15:44:11 -0300

    Adicionando Security Group AWS

--- main.tf
+++ main.tf
@@ -10,5 +10,22 @@ resource "aws_instance" "dev" {
     tags = {
         Name = "dev${count.index}"
     }
+    vpc_security_group_ids = [ "sg-02151ad397090c7e9" ]
+}
+
+resource "aws_security_group" "acesso-ssh" {
+    name = "acesso-ssh"
+    description = "acesso-ssh"
+
+    ingress {
+      from_port = 22
+      protocol = "tcp"
+      to_port = 22
+      cidr_blocks = ["190.89.142.142/32"]
+    }
+  
+  tags = {
+    Name = "ssh"
+  }
 }
 
