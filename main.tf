89d9997feb8b7683b2c351fa6bc0b28e9a46a78f
Silas Souza <silasdacosta@gmail.com>
2021-11-13 17:44:35 -0300

    Criando tabela dynamodb e dependencia com ec2 dev06 na us-east-2

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
     region = "us-east-1"
 }
 
+provider "aws" {
+    alias = "us-east-2"
+    region = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
     count = 1
     ami = "ami-083654bd07b5da81d"
@@ -38,6 +43,20 @@ resource "aws_instance" "dev5" {
     vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
 
+resource "aws_instance" "dev6" {
+    provider = aws.us-east-2
+    ami = "ami-0dd0ccab7e2801812"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+    tags = {
+        Name = "dev6"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
+    depends_on = [
+      aws_dynamodb_table.dynamodb-homologacao
+    ]
+}
+
 resource "aws_s3_bucket" "dev4" {
   bucket = "silassouza01-dev4"
   acl    = "private"
@@ -47,3 +66,20 @@ resource "aws_s3_bucket" "dev4" {
   }
 }
 
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+  provider = aws.us-east-2
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+}
--- security-group.tf
+++ security-group.tf
@@ -14,3 +14,20 @@ resource "aws_security_group" "acesso-ssh" {
     Name = "ssh"
   }
 }
+
+resource "aws_security_group" "acesso-ssh-us-east-2" {
+    provider = aws.us-east-2
+    name = "acesso-ssh"
+    description = "acesso-ssh"
+
+    ingress {
+      from_port = 22
+      protocol = "tcp"
+      to_port = 22
+      cidr_blocks = ["190.89.142.142/32"]
+    }
+  
+  tags = {
+    Name = "ssh"
+  }
+}
